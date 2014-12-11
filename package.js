8d1082e174dbdc4a4d160f152555895e0ee0b4c5
Michael Klein <klein@monbro.de>
2014-12-11 23:31:29 +0000

    bugfix

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'monbro:iron-router-breadcrumb',
   summary: 'This package will provide a easy way to add a breadcrumb to Iron.Router with enough flexibility.',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/monbro/meteor-breadcrumb-plugin/'
 });
 
@@ -11,9 +11,28 @@ Package.onUse(function(api) {
 
   api.use([
     'templating',
+    'meteor',
     'ui',
     "iron:core@1.0.0",
   ], "client");
+
+  // main namespace and utils
+  api.use('iron:core@1.0.0');
+
+  // ui layout
+  api.use('iron:layout@1.0.0');
+
+  // connect like middleware stack for client/server
+  api.use('iron:middleware-stack@1.0.0');
+
+  // client and server side url utilities and compiling
+  api.use('iron:url@1.0.0');
+
+  // for reactive urls and pushState in the browser
+  api.use('iron:location@1.0.0');
+
+  // for RouteController which inherits from this
+  api.use('iron:controller@1.0.0');
 
   api.export('Breadcrumb', ['client', 'server']);
 });
